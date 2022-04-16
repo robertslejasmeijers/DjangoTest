311be5c158734d297e47df1d478fb38b05d24546
Roberts Lejasmeijers <roberts.lejasmeijers@gmail.com>
2022-04-16 19:04:06 +0300

    37

--- rmscraper/urls.py
+++ rmscraper/urls.py
@@ -1,4 +1,4 @@
-"""djangotest URL Configuration
+"""rmscraper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
