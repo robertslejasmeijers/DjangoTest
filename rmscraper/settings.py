68d78aeac18db13661e85ba709018fff70ab8b29
Roberts Lejasmeijers <roberts.lejasmeijers@gmail.com>
2022-04-24 14:15:16 +0300

    50

--- rmscraper/settings.py
+++ rmscraper/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qqpof1b&^y8od62!o=@l0ahu69%3+vt_daosx5@t$1c+)xs8e0'
-
 # Application definition
 
 INSTALLED_APPS = [
